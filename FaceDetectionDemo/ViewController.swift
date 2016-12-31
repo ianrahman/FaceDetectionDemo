9e7c0c16ac64e612aa50da7d76cc60de01936a2e
Ian Alexander Rahman <ian.alexander.rahman@gmail.com>
2016-12-31 15:51:44 -0500

    Initial commit

--- FaceDetectionDemo/ViewController.swift
+++ FaceDetectionDemo/ViewController.swift
@@ -7,19 +7,115 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    let imageView = UIImageView()
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(imageView)
+        imageView.frame = view.frame
+        
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target:imagePicker, action:#selector(self.imageTapped(_:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
+        selectNewImage()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imageTapped(_ sender: UITapGestureRecognizer) {
+        selectNewImage()
     }
-
-
+    
+    func selectNewImage() {
+        present(imagePicker, animated: true, completion: {
+            print("new image")
+        })
+    }
+    
+    // Detect and highlight any faces in selected image
+    func detect() {
+        
+        guard let image = CIImage(image: imageView.image!) else { return }
+        
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        guard let faces = faceDetector?.features(in: image) else { return }
+        
+        // Convert the Core Image Coordinates to UIView Coordinates
+        let imageSize = image.extent.size
+        var transform = CGAffineTransform(scaleX: 1, y: -1)
+        transform = transform.translatedBy(x: 0, y: -imageSize.height)
+        
+        for face in faces as! [CIFaceFeature] {
+            
+            print("Found bounds are \(face.bounds)")
+            
+            // Apply the transform to convert the coordinates
+            var faceViewBounds = face.bounds.applying(transform)
+            
+            // Calculate the actual position and size of the rectangle in the image view
+            let viewSize = imageView.bounds.size
+            let scale = min(viewSize.width / imageSize.width,
+                            viewSize.height / imageSize.height)
+            let offsetX = (viewSize.width - imageSize.width * scale) / 2
+            let offsetY = (viewSize.height - imageSize.height * scale) / 2
+            
+            faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+            faceViewBounds.origin.x += offsetX
+            faceViewBounds.origin.y += offsetY
+            
+            let faceBox = UIView(frame: faceViewBounds)
+            
+            
+            faceBox.layer.borderWidth = 3
+            faceBox.layer.borderColor = UIColor.red.cgColor
+            faceBox.backgroundColor = UIColor.clear
+            imageView.addSubview(faceBox)
+            
+            if face.hasLeftEyePosition {
+                print("Left eye bounds are \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Right eye bounds are \(face.rightEyePosition)")
+            }
+        }
+    }
+    
 }
 
+// MARK: - UIImagePickerControllerDelegate Methods
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var newImage: UIImage
+        if let possibleImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+        OperationQueue.main.addOperation {
+            self.imageView.contentMode = .scaleToFill
+            self.imageView.image = newImage
+            self.detect()
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
